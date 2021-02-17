5000337bf52ff91fa7c90142a8b6acd006e9e344
Ahmad Horyzat <171059@ppu.edu.ps>
2021-02-17 15:21:55 +0200

    api for register user

---
+++ API/Controllers/AccounController.cs
@@ -0,0 +1,50 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if (await IsUserExist(registerDto.UserName)) return BadRequest("User Name is taken");
+
+            using var hmac = new HMACSHA512();
+            AppUser user = new AppUser
+            {
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        private async Task<bool> IsUserExist(string userName)
+        {
+            return await _context.Users.AnyAsync(user => user.UserName == userName.ToLower());
+        }
+    }
+}
